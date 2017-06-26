e58930fce1816f4c6bb0866c66b3d63f0163d7ef
elliot richardson <ear78@aol.com>
2017-06-26 13:43:14 -0500

    added 3 components and linked up to main app module for viewing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component'
+import { NavbarComponent } from './components/navbar/navbar.component'
+import { AboutComponent } from './components/about/about.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    NavbarComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
